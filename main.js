26abefd4cafd0f7d19c3d58d630b999cc66929c2
duykhue257 <nguyenduykhue257@gmail.com>
2022-06-12 22:26:01 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
+// const button = document.getElementById("button");
+// const menu = document.getElementById("main-menu");
+
+// button.addEventListener("click", function () {
+//     menu.classList.toggle("hidden");
+// });
